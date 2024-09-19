74009bd107dc0c53cc64dfe78bbd22e8a2ef3d27
delfin <carljefferson.delfin@gmail.com>
2024-09-19 17:19:17 +0800

    adjust padding

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
         className="w-full h-full object-cover"
       />
       <div className="absolute top-1 left-0 w-full h-full bg-gradient-to-t from-black border-0">
-        <div className="container mx-auto flex flex-col justify-end h-full w-full p-4 pb-10 lg:px-10 lg:pb-14 space-y-2 lg:space-y-4">
+        <div className="container mx-auto flex flex-col justify-end h-full w-full p-4 pb-14 lg:px-10 lg:pb-20 space-y-2 lg:space-y-4">
           <span className="text-2xl lg:text-5xl font-bold">
             Southville 8B Cemetery
           </span>
